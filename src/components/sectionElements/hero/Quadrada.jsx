35b9704be2639b5238265a7482cf3dc6c56efb32
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-07-04 15:28:29 -0300

    fix: Navbar e Hero

--- src/components/sectionElements/hero/Quadrada.jsx
+++ src/components/sectionElements/hero/Quadrada.jsx
@@ -25,7 +25,7 @@ export default function Quadrada({ appDownloadButtons, colorMode }) {
     >
       <div className="relative z-10 flex w-full items-left">
         <div className="w-full  justify-evenly">
-          <div className="h-[100px] phone2:h-[110px] phone3:h-[100px] desktop3:h-[120px] " />{" "}
+          <div className="h-[130px] phone2:h-[130px] phone3:h-[130px] desktop3:h-[120px] " />{" "}
           <div className="flex flex-col-reverse gap-[40px] desktop1:flex-row desktop1:justify-between mx-auto w-[90%] max-w-[1215px] items-center pb-[64px] desktop1:pt-[68px] desktop1:pb-[96px]">
             <div className="flex flex-col w-full desktop1:w-[50%] desktop1:mr-[20px]">
               {/* MiniTag preta */}
